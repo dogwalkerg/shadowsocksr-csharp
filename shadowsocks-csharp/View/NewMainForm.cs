0bc5774f42cad03d09d111ced558fb50de1217cc
username <username@github.com>
2018-06-13 18:04:30 +0800

    add file

---
+++ shadowsocks-csharp/View/NewMainForm.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Threading;
+using Shadowsocks.Controller;
+using Shadowsocks.Model;
+using Shadowsocks.Util;
+using Shadowsocks.Properties;
+
+namespace Shadowsocks.View
+{
+    public partial class NewMainForm : Form
+    {
+        private ShadowsocksController controller;
+        // this is a copy of configuration that we are working on
+        private Configuration _modifiedConfiguration;
+        private ServerLogForm serverLogForm;
+        private ConfigForm configForm;
+        private UpdateChecker updateChecker;
+        public delegate void treeinvoke();
+
+        public NewMainForm(ShadowsocksController controller)
+        {
+            this.Font = System.Drawing.SystemFonts.MessageBoxFont;
+            InitializeComponent();
+            this.Icon = Icon.FromHandle(Resources.ssw128.GetHicon());
+            this.controller = controller;
+            LoadCurrentConfiguration();
+            UpdateTexts();
+            LoadServer();
+            updateChecker = new UpdateChecker();
+            //Thread t = new Thread(new ThreadStart(show_ping_start));
+            //t.Start();
+        }
+
+        private void LoadCurrentConfiguration()
+        {
+            _modifiedConfiguration = controller.GetConfiguration();
+        }
+
+        private void UpdateTexts()
+        {
+            //Configuration config = controller.GetCurrentConfiguration();
+            this.Text = I18N.GetString("NewMainForm");
+            label1.Text = I18N.GetString("Connect_type");
+            button1.Text = I18N.GetString("Connect");
+            button2.Text = I18N.GetString("Test");
+            button3.Text = I18N.GetString("Statistics");
+            button4.Text = I18N.GetString("Server_config");
+        }
+        
+        private void LoadServer()
+        {
+            Configuration c = controller.GetConfiguration();
+            for (int i=0; i < c.configs.Count; i++)
+            {
+                Server s = c.configs[i];
+                string[] item = { s.FriendlyName(), "", "" };
+                ListViewItem itm = new ListViewItem(item);
+                //itm.BackColor = Color.Silver;
+                //itm.ForeColor = Color.White;
+                //itm.
+                if (c.index == i)
+                {
+                    itm.BackColor = Color.FromArgb(10, 36, 106);
+                    itm.ForeColor = Color.White;
+                }
+                listView1.Items.Add(itm);
+            }
+            switch (c.sysProxyMode)
+            {
+                case 1:
+                    comboBox1.SelectedIndex = 0;
+                    label2.Text = "请注意！socks 模式将不会设置IE代理，\n\n" +
+                    "也即无法直接通过浏览器代理上外网！\n\n" +
+                    "您可将连接模式修改为 pac模式 或\n\n" +
+                    "全局模式 来通过浏览器上外网（新人无需关注两者区别）！";
+                    break;
+                case 2:
+                    comboBox1.SelectedIndex = 1;
+                    break;
+                case 3:
+                    comboBox1.SelectedIndex = 2;
+                    break;
+            }
+
+        }
+
+        private void DisconnectCurrent(object sender, EventArgs e)
+        {
+            Configuration config = controller.GetCurrentConfiguration();
+            for (int id = 0; id < config.configs.Count; ++id)
+            {
+                Server server = config.configs[id];
+                server.GetConnections().CloseAll();
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("hi");
+            if (listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            else if (listView1.SelectedItems.Count == 1)
+            {
+                //MessageBox.Show(Convert.ToString(listView1.SelectedItems.Count));
+                //_modifiedConfiguration.index = listView1.SelectedItems[0].Index;
+                //_modifiedConfiguration
+                
+                //int this_index = listView1.SelectedItems[0].Index;
+                
+                //controller.SelectServerIndex(this_index);
+                //listView1.Items[this_index].BackColor = Color.Silver;
+                //DisconnectCurrent(this, new EventArgs());
+            }
+            
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            else if (listView1.SelectedItems.Count == 1)
+            {
+                //MessageBox.Show(Convert.ToString(listView1.SelectedItems.Count));
+                //_modifiedConfiguration.index = listView1.SelectedItems[0].Index;
+                //_modifiedConfiguration
+
+                int last_index = controller.GetConfiguration().index;
+                int this_index = listView1.SelectedItems[0].Index;
+
+                listView1.Items[last_index].BackColor = Color.White;
+                listView1.Items[last_index].ForeColor = Color.Black;
+                controller.SelectServerIndex(this_index);
+                listView1.Items[this_index].BackColor = Color.FromArgb(10, 36, 106);
+                listView1.Items[this_index].ForeColor = Color.White;
+                DisconnectCurrent(this, new EventArgs());
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Configuration c = controller.GetConfiguration();
+            
+            if (listView1.SelectedItems.Count>0)
+            {
+                int last_index = controller.GetConfiguration().index;
+                int this_index = listView1.SelectedItems[0].Index;
+
+                if (last_index != this_index)
+                {
+                    listView1.Items[last_index].BackColor = Color.White;
+                    listView1.Items[last_index].ForeColor = Color.Black;
+                    controller.SelectServerIndex(this_index);
+                    listView1.Items[this_index].BackColor = Color.FromArgb(10, 36, 106);
+                    listView1.Items[this_index].ForeColor = Color.White;
+                    DisconnectCurrent(this, new EventArgs());
+                }
+            }
+
+            if (comboBox1.SelectedIndex != c.sysProxyMode - 1)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    label2.Text = "请注意！socks 模式将不会设置IE代理，\n\n" +
+                    "也即无法直接通过浏览器代理上外网！\n\n" +
+                    "您可将连接模式修改为 pac模式 或\n\n" +
+                    "全局模式 来通过浏览器上外网（新人无需关注两者区别）！";
+                }
+                else
+                {
+                    label2.Text = "";
+                }
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        controller.ToggleMode(ProxyMode.Direct);
+                        break;
+                    case 1:
+                        controller.ToggleMode(ProxyMode.Pac);
+                        break;
+                    case 2:
+                        controller.ToggleMode(ProxyMode.Global);
+                        break;
+                }
+            }
+        }
+
+        private void show_ping_start()
+        {
+            Thread.Sleep(5000);
+            Configuration c = controller.GetConfiguration();
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                //Console.WriteLine("1: " + Convert.ToString(i));
+                string ip = c.configs[i].server;
+                int port = c.configs[i].server_port;
+
+                my_param m = new my_param();
+                m.i = i;
+                m.ip = ip;
+                m.method = 0;
+                Thread t = new Thread(new ParameterizedThreadStart(inthread));
+                t.Start(m);
+                my_param mm = new my_param();
+                mm.i = i;
+                mm.ip = ip;
+                mm.port = port;
+                mm.method = 1;
+                Thread tt = new Thread(new ParameterizedThreadStart(inthread));
+                tt.Start(mm);
+            }
+        }
+
+        private void show_ping()
+        {
+            
+            Configuration c = controller.GetConfiguration();
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                //Console.WriteLine("1: " + Convert.ToString(i));
+                string ip = c.configs[i].server;
+                int port = c.configs[i].server_port;
+
+                my_param m = new my_param();
+                m.i = i;
+                m.ip = ip;
+                m.method = 0;
+                Thread t = new Thread(new ParameterizedThreadStart(inthread));
+                t.Start(m);
+                my_param mm = new my_param();
+                mm.i = i;
+                mm.ip = ip;
+                mm.port = port;
+                mm.method = 1;
+                Thread tt = new Thread(new ParameterizedThreadStart(inthread));
+                tt.Start(mm);
+            }
+        }
+        private void inthread(object obj)
+        {
+            my_param o = (my_param)obj;
+            if (o.method == 0)
+            {
+                string resp = Utils.ping_example_3(o.ip);
+                if (this.IsHandleCreated)
+                {
+                    listView1.BeginInvoke(new treeinvoke(() =>
+                    {
+                        listView1.Items[o.i].SubItems[1].Text = resp;
+                    }));
+                }
+                else
+                {
+                    return;
+                }
+                //listView1.BeginInvoke(new treeinvoke(() =>
+                //{
+                //    listView1.Items[o.i].SubItems[1].Text = resp;
+                //}));
+
+            }
+            else if (o.method == 1)
+            {
+                string resp = Utils.tcping_example_3(o.ip, o.port);
+                if (this.IsHandleCreated)
+                {
+                    listView1.BeginInvoke(new treeinvoke(() =>
+                    {
+                        listView1.Items[o.i].SubItems[2].Text = resp;
+                    }));
+                }
+                else
+                {
+                    return;
+                }
+                //listView1.BeginInvoke(new treeinvoke(() =>
+                //{
+                //    listView1.Items[o.i].SubItems[2].Text = resp;
+                //}));
+            }
+            //Console.WriteLine("inthread: " + Convert.ToString(o.i));
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Thread t = new Thread(new ThreadStart(show_ping));
+            //t.Start();
+            show_ping();
+        }
+
+        void serverLogForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serverLogForm = null;
+            Util.Utils.ReleaseMemory();
+        }
+        void configForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            configForm = null;
+            Util.Utils.ReleaseMemory();
+        }
+        private void ShowServerLogForm()
+        {
+            if (serverLogForm != null)
+            {
+                serverLogForm.Activate();
+                serverLogForm.Update();
+                if (serverLogForm.WindowState == FormWindowState.Minimized)
+                {
+                    serverLogForm.WindowState = FormWindowState.Normal;
+                }
+            }
+            else
+            {
+                serverLogForm = new ServerLogForm(controller);
+                serverLogForm.Show();
+                serverLogForm.Activate();
+                serverLogForm.BringToFront();
+                serverLogForm.FormClosed += serverLogForm_FormClosed;
+            }
+        }
+        private void ShowConfigForm(bool addNode)
+        {
+            if (configForm != null)
+            {
+                configForm.Activate();
+                // MessageBox.Show("1");
+                if (addNode)
+                {
+                    Configuration cfg = controller.GetCurrentConfiguration();
+                    configForm.SetServerListSelectedIndex(cfg.index + 1);
+                }
+            }
+            else
+            {
+                //MessageBox.Show("2.0");
+                configForm = new ConfigForm(controller, updateChecker, addNode ? -1 : -2);
+                //MessageBox.Show("2.1");
+                configForm.Show();
+                //MessageBox.Show("2.2");
+                configForm.Activate();
+                configForm.BringToFront();
+                configForm.FormClosed += configForm_FormClosed;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShowServerLogForm();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ShowConfigForm(false);
+        }
+    }
+
+    class my_param
+    {
+        public string ip;
+        public Int32 port;
+        public int i;
+        public int method;
+    }
+}
---
+++ shadowsocks-csharp/View/NewMainForm.designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Shadowsocks.View
+{
+    partial class NewMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(357, 386);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Server";
+            this.columnHeader1.Width = 166;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ping";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 54;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "tcping";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 59;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "socks模式",
+            "pac模式",
+            "全局模式"});
+            this.comboBox1.Location = new System.Drawing.Point(539, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(139, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(403, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Connect_type";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(462, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(529, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Statistics";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(641, 291);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Server_config";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // NewMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 382);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "NewMainForm";
+            this.Text = "NewMainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
